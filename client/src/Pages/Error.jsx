4f0d01086d51ffa3f16f624f9059077cc023a5a6
Saksham Verma <sakshamverma1000@gmail.com>
2024-11-23 11:53:24 +0530

    Making error page responsive for mobile devices

--- client/src/Pages/Error.jsx
+++ client/src/Pages/Error.jsx
@@ -1,12 +1,14 @@
-import { Button, Stack, Typography } from "@mui/material";
+import { Button, Stack, Typography, useMediaQuery } from "@mui/material";
 import React from "react";
 import { Link } from "react-router-dom";
 const Error = ({ setHeaderStatus }) => {
   setHeaderStatus("not visible");
+  const _700 = useMediaQuery("(min-width: 700px)");
   return (
     <>
       <Stack
         width={"100vw"}
+        bgcolor="antiquewhite"
         height={"100vh"}
         flexDirection={"row"}
         justifyContent={"center"}
@@ -15,23 +17,27 @@ const Error = ({ setHeaderStatus }) => {
       >
         <Stack
           p={5}
+          bgcolor="green"
           mt={15}
           flexDirection={"column"}
           alignItems={"center"}
           borderRadius={"10px"}
           gap={5}
         >
-          <Typography variant="h2">Sorry, this page isn't available</Typography>
-          <Typography variant="h4">
+          <Typography sx={{ fontSize: _700 ? "4vw" : "3vw" }} variant="h3">
+            Sorry, this page isn't available
+          </Typography>
+          <Typography sx={{ fontSize: _700 ? "2.5vw" : "2.5vw" }} variant="h4">
             The link you followed may be broken , or the page may have been
             removed.
           </Typography>
           <Link to="/">
             <Button
               href="/"
-              size="large"
+              size={_700 ? "large" : "small"}
               sx={{
-                p: 2,
+                fontSize: _700 ? "2.4vw" : "2vw",
+                p: _700 ? 2 : 1.5,
                 bgcolor: "blue",
                 color: "white",
                 borderRadius: "10px",
